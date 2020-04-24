4dadd3807755892b517af4c22258283856d3e2d3
水木佑哉 <mizukiyuukana@mizukiyuunarinoMacBook-Pro.local>
2020-04-24 21:48:41 +0900

    add termbox sample

--- main.go
+++ main.go
@@ -3,10 +3,40 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/nsf/termbox-go"
 	"github.com/urfave/cli"
 )
 
+func drawLine(x, y int, str string) {
+	color := termbox.ColorDefault
+	backgroundColor := termbox.ColorDefault
+	runes := []rune(str)
+
+	for i := 0; i < len(runes); i += 1 {
+		termbox.SetCell(x+i, y, runes[i], color, backgroundColor)
+	}
+}
+func draw() {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	drawLine(0, 0, "press ESC to exit.")
+	drawLine(2, 1, fmt.Sprintf("date: %v", time.Now()))
+	termbox.Flush()
+}
+func tetris() {
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				return
+			}
+		default:
+			draw()
+		}
+	}
+}
 func main() {
 	app := cli.NewApp()
 	app.Name = "TetrisCLI"
@@ -14,6 +44,12 @@ func main() {
 	app.Version = "0.0.1"
 	app.Action = func(context *cli.Context) error {
 		fmt.Println(context.Args().Get(0))
+		err := termbox.Init()
+		if err != nil {
+			panic(err)
+		}
+		defer termbox.Close()
+		tetris()
 		return nil
 	}
 	app.Run(os.Args)
