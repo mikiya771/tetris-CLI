b086700762e6812334b208158d180be8d34ea4ad
progfay <shun1856shun@gmail.com>
2020-05-02 23:38:16 +0900

    test: fix variables name

--- tetrimino/tetrimino_test.go
+++ tetrimino/tetrimino_test.go
@@ -12,16 +12,16 @@ import (
 func TestNewTetrimino(t *testing.T) {
 	tests := []struct {
 		name             string
-		tetriminoType    TetriminoType
+		shape            ShapeType
 		expectedPosture  Posture
 		expectedPosition Position
 		expectedBlocks   BlockPositions
 	}{
-		{"Iミノを作る", I_SHAPE, DEG0, Position{4, 0}, BlockPositions{{3, 0}, {4, 0}, {5, 0}, {6, 0}}},
+		{"Iミノを作る", IShape, DEG0, Position{4, 0}, BlockPositions{{3, 0}, {4, 0}, {5, 0}, {6, 0}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tetriminoType := tt.tetriminoType
+			tetriminoType := tt.shape
 			tetrimino := NewTetrimino(tetriminoType)
 			assert.Equal(t, tt.expectedPosition, tetrimino.Pos)
 			assert.Equal(t, tt.expectedPosture, tetrimino.Rot)
@@ -33,7 +33,7 @@ func TestNewTetrimino(t *testing.T) {
 func TestApplyAction(t *testing.T) {
 	tests := []struct {
 		name             string
-		tetriminoType    TetriminoType
+		shape            ShapeType
 		initPosition     Position
 		initPosture      Posture
 		actionType       ActionType
@@ -41,15 +41,15 @@ func TestApplyAction(t *testing.T) {
 		expectedPosition Position
 		expectedBlocks   BlockPositions
 	}{
-		{"Iミノを左に移動する", I_SHAPE, Position{4, 0}, DEG0, MOVE_LEFT, DEG0, Position{3, 0}, BlockPositions{{2, 0}, {3, 0}, {4, 0}, {5, 0}}},
+		{"Iミノを左に移動する", IShape, Position{4, 0}, DEG0, MoveLeftAction, DEG0, Position{3, 0}, BlockPositions{{2, 0}, {3, 0}, {4, 0}, {5, 0}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tetriminoType := tt.tetriminoType
+			tetriminoType := tt.shape
 			tetrimino := NewTetrimino(tetriminoType)
 			tetrimino.Pos = tt.initPosition
 			tetrimino.Rot = tt.initPosture
-			tetrimino.ApplyAction(MOVE_LEFT)
+			tetrimino.ApplyAction(MoveLeftAction)
 			assert.Equal(t, tt.expectedPosition, tetrimino.Pos)
 			assert.Equal(t, tt.expectedPosture, tetrimino.Rot)
 			assert.Equal(t, tt.expectedBlocks, tetrimino.BlockPoss)
