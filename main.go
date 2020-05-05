9df0a8140fee8144035ab8001b8b59b5107ce9b7
progfay <shun1856shun@gmail.com>
2020-05-06 02:28:23 +0900

    refactor: split view and store

--- main.go
+++ main.go
@@ -4,48 +4,14 @@ import (
 	"github.com/nsf/termbox-go"
 	a "github.com/tetris-CLI/action"
 	dispatcher "github.com/tetris-CLI/dispatcher"
-
-	// st "github.com/tetris-CLI/stage"
-	s "github.com/tetris-CLI/store"
-	// tm "github.com/tetris-CLI/tetrimino"
+	_ "github.com/tetris-CLI/store"
 )
-
-func tetris() {
-	// tetrimino := tm.NewTetrimino(tm.IShape)
-	// var stage st.Stage
-	// for {
-	// 	if stage.IsGameOver() {
-	// 		break
-	// 	}
-	// 	if EvaluateTermination(tetrimino, stage) {
-	// 		for _, mino := range tetrimino.Minos {
-	// 			stage.SetMino(mino)
-	// 		}
-	// 		tetrimino = tm.NewTetrimino(tm.IShape)
-	// 	}
-	// }
-}
-
-// // EvaluateTermination ゲームオーバーか否かを判定する
-// func EvaluateTermination(tetrimino tm.Tetrimino, stage st.Stage) bool {
-// 	for _, mino := range tetrimino.Minos {
-// 		if mino.Y >= 19 {
-// 			return true
-// 		}
-// 		if stage.Lines[mino.Y+1].Cells[mino.X].IsFilled == true {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
 
 func main() {
 	err := termbox.Init()
 	if err != nil {
 		panic(err)
 	}
-	defer termbox.Close()
-	s.NewStore()
 
 	dispatcher.Dispatch(a.InitializeGameAction)
 	pollKeyEvent()
@@ -57,6 +23,7 @@ func pollKeyEvent() {
 		case termbox.EventKey:
 			switch ev.Key {
 			case termbox.KeyEsc:
+				termbox.Close()
 				return
 			case termbox.KeyArrowLeft:
 				dispatcher.Dispatch(a.MoveTetriminoToLeftAction)
